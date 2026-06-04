d1426885c1e7ba0c7e8a3213b4b3905364dea0b9
ALEXEI <wertikoo@yahoo.com>
2026-06-04 22:31:00 +0300

    Polish PWA splash screen and cache shell

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,55 @@
-const VERSION = '3.5-push';
+const VERSION = '3.6-cache';
+const APP_SHELL_CACHE = 'voicebridge-shell-' + VERSION;
+const APP_SHELL_FILES = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/apple-touch-icon.png'
+];
 
-self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(APP_SHELL_CACHE)
+      .then(cache => cache.addAll(APP_SHELL_FILES))
+      .catch(() => {})
+      .then(() => self.skipWaiting())
+  );
+});
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys()
-      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(keys => Promise.all(
+        keys
+          .filter(key => key.startsWith('voicebridge-shell-') && key !== APP_SHELL_CACHE)
+          .map(key => caches.delete(key))
+      ))
       .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+  if (request.method !== 'GET') return;
+  const url = new URL(request.url);
+  if (url.origin !== self.location.origin) return;
+  if (!APP_SHELL_FILES.includes(url.pathname) && url.pathname !== '/') return;
+
+  event.respondWith(
+    caches.match(request).then(cached => {
+      const fresh = fetch(request)
+        .then(response => {
+          if (response && response.ok) {
+            const copy = response.clone();
+            caches.open(APP_SHELL_CACHE).then(cache => cache.put(request, copy));
+          }
+          return response;
+        })
+        .catch(() => cached);
+      return cached || fresh;
+    })
   );
 });
 
